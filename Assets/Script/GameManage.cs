4a68bc906917f0df078d9d12eadec621bf284160
TheShen21 <bshen@roc-dev.com>
2021-10-07 15:15:48 +0200

    new scene an gameover screen

--- Assets/Script/GameManage.cs
+++ Assets/Script/GameManage.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManage : MonoBehaviour
 {
+    bool gameEnded = false; 
+
     public void GameOver()
     {
-        Debug.Log("u gay");
+        if(gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("u ded");
+            Over();
+        }
+
+    }
+    void Over()
+    {
+        SceneManager.LoadScene("GameOver");
     }
 }
